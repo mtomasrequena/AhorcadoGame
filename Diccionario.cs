019cd2bf017895423a05cb1c19a62185a624afac
LeanSpinardi <93681816+LeanSpinardi@users.noreply.github.com>
2021-11-08 11:02:15 -0300

    Se eliminó palabra de Diccionario

--- Diccionario.cs
+++ Diccionario.cs
@@ -6,7 +6,7 @@ namespace AhorcadoGame
 {
     class Diccionario
     {
-        private string[] diccionario = { "Marcos", "Leandro", "Jerkan", "ElizabethRiendo", "Matias", "cebra", "durazno", "sombrero", "Pato", "almeja", "HOLA","chau","harta","odioesto"};
+        private string[] diccionario = { "Marcos", "Leandro", "Jerkan", "ElizabethRiendo", "Matias", "cebra", "durazno", "sombrero", "Pato", "almeja", "HOLA","chau","harta"};
 
         public string generarPalabra()
         {
