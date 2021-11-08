b86008aad1622940ed6aed5ef093509db4cdf72f
Leandro spinardi <leanspinardi@hotmail.com>
2021-11-08 11:21:08 -0300

    Se agregó el metodo "mostrarReglas()" en la clase Introduccion

--- Introduccion.cs
+++ Introduccion.cs
@@ -15,11 +15,9 @@ namespace AhorcadoGame
 
             Console.Clear();
 
-            Console.WriteLine("Bienvenido {0}", this.nombre);
+            Console.WriteLine("\nBienvenido {0}\n", this.nombre);
 
-            // si quieren dejamos aca
-
-            
+            mostrarReglas();
         }
 
         public string getNombre()
@@ -31,6 +29,15 @@ namespace AhorcadoGame
         {
             Introduccion j1 = new Introduccion();
             return j1;
+        }
+
+        private void mostrarReglas()
+        {
+            Console.WriteLine("REGLAS: \n");
+            Console.WriteLine("- Inicias el juego con 6 vidas.");
+            Console.WriteLine("- Se le mostrara una palabra oculta y deberas ingresar una letra que creas contiene la palabra.");
+            Console.WriteLine("- Pierdes una vida si la letra ingresada no esta en la palabra. Si pierdes todas las vidas pierdes el juego.");
+            Console.WriteLine("- Si completas la palabra, ganas el juego.");
         }
     }
 }
