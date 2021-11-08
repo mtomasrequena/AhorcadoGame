0f6d86ea329dcc569a7a831675a65ae97748c10f
QuintanaElizabeth <elizabethdenisequintana@gmail.com>
2021-11-08 00:30:57 -0300

    agregue palabras por enecima vez

--- Diccionario.cs
+++ Diccionario.cs
@@ -6,7 +6,7 @@ namespace AhorcadoGame
 {
     class Diccionario
     {
-        private string[] diccionario = { "Marcos", "Leandro", "Jerkan", "ElizabethRiendo", "Matias", "cebra", "durazno", "sombrero", "Pato", "almeja", "HOLA"};
+        private string[] diccionario = { "Marcos", "Leandro", "Jerkan", "ElizabethRiendo", "Matias", "cebra", "durazno", "sombrero", "Pato", "almeja", "HOLA","chau","harta"};
 
         public string generarPalabra()
         {
